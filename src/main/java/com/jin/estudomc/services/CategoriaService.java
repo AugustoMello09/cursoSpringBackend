21687e3237960e6891ecbd3dd098555613c7e565
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-09 09:57:28 -0300

    Atualizando categoria com PUT, outros pequenos ajustes

--- src/main/java/com/jin/estudomc/services/CategoriaService.java
+++ src/main/java/com/jin/estudomc/services/CategoriaService.java
@@ -26,4 +26,10 @@ public class CategoriaService {
 		obj.setId(null);
 		return repo.save(obj);
 	}
+
+	public Categoria update(Categoria obj) {
+		find(obj.getId());
+		return repo.save(obj);
+	}
+
 }
