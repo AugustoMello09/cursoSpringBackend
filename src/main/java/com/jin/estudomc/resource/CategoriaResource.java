25a8bd9474dadc679e36ed27fccfa3853cec24d7
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-10 09:28:37 -0300

    Paginacao com paremetos opcionais na requisicao

--- src/main/java/com/jin/estudomc/resource/CategoriaResource.java
+++ src/main/java/com/jin/estudomc/resource/CategoriaResource.java
@@ -5,11 +5,13 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -60,6 +62,20 @@ public class CategoriaResource {
 	List<Categoria> list = service.findAll();
 	// vai ter que percorrer a list e para cada elemento da lista vai instanciar o DTO correspondente(converter para DTO) 
 	List<CategoriaDTO>	listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+	return ResponseEntity.ok().body(listDto);
+
+	}
+
+	@RequestMapping(value="/pages", method = RequestMethod.GET)
+	public ResponseEntity<Page<CategoriaDTO>> findPage(
+			@RequestParam(value ="page", defaultValue="0")Integer page, 
+			@RequestParam(value ="linesPerPage", defaultValue="24")Integer linesPerPage,
+			@RequestParam(value ="orderBy", defaultValue="nome")String orderBy,
+			@RequestParam(value ="direction", defaultValue="ASC")String direction) {
+	// busca as paginas de categoria do Banco
+	Page<Categoria> list = service.findPage(page, linesPerPage, orderBy, direction);
+	// vai ter que percorrer as páginas e para cada elemento da vai instanciar o DTO correspondente(converter para DTO) 
+	Page<CategoriaDTO>	listDto = list.map(obj -> new CategoriaDTO(obj));
 	return ResponseEntity.ok().body(listDto);
 
 	}
