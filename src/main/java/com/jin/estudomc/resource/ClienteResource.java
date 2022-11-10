ca812acb82f2d70198a1724e5bb4cb7eb98b1856
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-10 14:57:20 -0300

    PUT, DELETE e GET para cliente

--- src/main/java/com/jin/estudomc/resource/ClienteResource.java
+++ src/main/java/com/jin/estudomc/resource/ClienteResource.java
@@ -1,13 +1,22 @@
 package com.jin.estudomc.resource;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.jin.estudomc.domain.Cliente;
+import com.jin.estudomc.dto.ClienteDTO;
 import com.jin.estudomc.services.ClienteService;
 
 @RestController
@@ -22,5 +31,45 @@ public class ClienteResource {
 		Cliente obj = service.find(id);
 		return ResponseEntity.ok().body(obj);
 	
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.PUT)
+	public ResponseEntity<Void> update(@Valid @RequestBody ClienteDTO objDto, @PathVariable Integer id) {
+		Cliente obj = service.fromDTO(objDto);
+		obj.setId(id);
+		obj = service.update(obj);
+		return ResponseEntity.noContent().build();
+	}
+
+	@RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+	public ResponseEntity<Void> delete(@PathVariable Integer id) {
+		service.delete(id);
+
+		return ResponseEntity.noContent().build();
+
+	}
+
+	@RequestMapping( method = RequestMethod.GET)
+	public ResponseEntity<List<ClienteDTO>> findAll() {
+	// busca as listas de categoria do Banco
+	List<Cliente> list = service.findAll();
+	// vai ter que percorrer a list e para cada elemento da lista vai instanciar o DTO correspondente(converter para DTO) 
+	List<ClienteDTO>	listDto = list.stream().map(obj -> new ClienteDTO(obj)).collect(Collectors.toList());
+	return ResponseEntity.ok().body(listDto);
+
+	}
+
+	@RequestMapping(value="/pages", method = RequestMethod.GET)
+	public ResponseEntity<Page<ClienteDTO>> findPage(
+			@RequestParam(value ="page", defaultValue="0")Integer page, 
+			@RequestParam(value ="linesPerPage", defaultValue="24")Integer linesPerPage,
+			@RequestParam(value ="orderBy", defaultValue="nome")String orderBy,
+			@RequestParam(value ="direction", defaultValue="ASC")String direction) {
+	// busca as paginas de categoria do Banco
+	Page<Cliente> list = service.findPage(page, linesPerPage, orderBy, direction);
+	// vai ter que percorrer as páginas e para cada elemento da vai instanciar o DTO correspondente(converter para DTO) 
+	Page<ClienteDTO>	listDto = list.map(obj -> new ClienteDTO(obj));
+	return ResponseEntity.ok().body(listDto);
+
 	}
 }
