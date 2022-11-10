0ec546638771e9a97d27f3d58943e075ca9dc14e
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-10 11:48:45 -0300

    Personalizando o retorno da validacao

---
+++ src/main/java/com/jin/estudomc/resource/exception/FieldMessage.java
@@ -0,0 +1,37 @@
+package com.jin.estudomc.resource.exception;
+
+import java.io.Serializable;
+
+public class FieldMessage implements Serializable {
+	private static final long serialVersionUID = 1L;
+
+	private String fieldName;
+	private String message;
+
+	public FieldMessage() {
+
+	}
+
+	public FieldMessage(String fieldName, String message) {
+		super();
+		this.fieldName = fieldName;
+		this.message = message;
+	}
+
+	public String getFielName() {
+		return fieldName;
+	}
+
+	public void setFielName(String fieldName) {
+		this.fieldName = fieldName;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+}
