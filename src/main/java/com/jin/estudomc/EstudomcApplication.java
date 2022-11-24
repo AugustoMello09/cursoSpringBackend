b14e8a5888c270702ce1de38b21bbce2761eba0d
José Augusto <joseaugusto.mello01@gmail.com>
2022-11-24 19:22:23 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/jin/estudomc/EstudomcApplication.java
+++ src/main/java/com/jin/estudomc/EstudomcApplication.java
@@ -1,14 +1,18 @@
 package com.jin.estudomc;
 
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.jin.estudomc.services.S3Service;
+
 @SpringBootApplication
 public class EstudomcApplication implements CommandLineRunner {
 	
-	
+	@Autowired
+	private S3Service s3Service;
 	
 	public static void main(String[] args) {
 		SpringApplication.run(EstudomcApplication.class, args);
@@ -16,7 +20,7 @@ public class EstudomcApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {	
-		
+		s3Service.uploadFile("D:\\fotos\\jdm");
 	}
 
 }
